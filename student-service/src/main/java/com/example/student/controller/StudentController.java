2173983ad76492e07e5aa3c05bfac3565240bf02
haresh <haresh268@gmail.com>
2026-07-30 22:06:29 +0530

    log4j

--- student-service/src/main/java/com/example/student/controller/StudentController.java
+++ student-service/src/main/java/com/example/student/controller/StudentController.java
@@ -5,8 +5,9 @@ import java.util.Arrays;
 import java.util.List;
 import io.github.resilience4j.ratelimiter.RequestNotPermitted;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import lombok.extern.slf4j.Slf4j;
 
-
+@Slf4j
 @RestController
 @RequestMapping("/api/students")
 public class StudentController {
@@ -17,10 +18,12 @@ public class StudentController {
     @RateLimiter(name = "getAllStudents", fallbackMethod = "fallback")
     @GetMapping("/getAllStudents")
     public List<String> getAllStudents() {
+        log.info ("welecome to student service");
         return Arrays.asList("Alice Johnson (Port: " + port + ")", "Bob Smith", "Charlie Brown");
     }
 
     public List<String>  fallback(RequestNotPermitted ex) {
+        log.info ("welecome to fullback method");
         return  Arrays.asList("Too many requests. Try again later." );
     }
 }
