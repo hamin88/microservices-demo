80e41955fa1a3dd699e1888f1c31e9d991ebc46d
haresh <haresh268@gmail.com>
2026-07-30 21:48:56 +0530

    disabled eureka , rate limit implemented

--- student-service/src/main/java/com/example/student/StudentApplication.java
+++ student-service/src/main/java/com/example/student/StudentApplication.java
@@ -1,20 +1,20 @@
 package com.example.student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class StudentApplication {
     public static void main(String[] args) {
         SpringApplication.run(StudentApplication.class, args);
     }
 
     @Bean
-    @LoadBalanced
+    //@LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
--- student-service/src/main/java/com/example/student/controller/StudentController.java
+++ student-service/src/main/java/com/example/student/controller/StudentController.java
@@ -3,6 +3,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Value;
 import java.util.Arrays;
 import java.util.List;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+
 
 @RestController
 @RequestMapping("/api/students")
@@ -11,8 +14,13 @@ public class StudentController {
     @Value("${server.port}")
     private String port;
 
-    @GetMapping
+    @RateLimiter(name = "getAllStudents", fallbackMethod = "fallback")
+    @GetMapping("/getAllStudents")
     public List<String> getAllStudents() {
         return Arrays.asList("Alice Johnson (Port: " + port + ")", "Bob Smith", "Charlie Brown");
+    }
+
+    public List<String>  fallback(RequestNotPermitted ex) {
+        return  Arrays.asList("Too many requests. Try again later." );
     }
 }
